880c3baa834968b4c3bfb66d30014e553910b8b2
HammaBG <mohamedamine00879@gmail.com>
2024-12-20 12:37:48 +0100

    enhance sidebar footer with dropdown menu for user account options

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { SidebarProvider, Sidebar, SidebarContent, SidebarHeader, SidebarFooter, SidebarMenu, SidebarMenuItem, SidebarMenuButton, SidebarGroup} from './components/ui/sidebar';
-import { TiHome } from "react-icons/ti";
+import { SidebarProvider, Sidebar, SidebarContent, SidebarHeader, SidebarFooter, SidebarMenu, SidebarMenuItem, SidebarMenuButton, SidebarGroup } from './components/ui/sidebar';
+import { TiHome , TiThMenuOutline } from "react-icons/ti";
 import { CgProfile } from "react-icons/cg";
 import { FaCar } from "react-icons/fa";
 import {
@@ -34,18 +34,25 @@ function App() {
               <SidebarMenuButton>Item 2</SidebarMenuButton>
             </SidebarMenuItem>
           </SidebarMenu>
-          </SidebarContent>
+        </SidebarContent>
         <SidebarFooter>
-        <DropdownMenu>
-   
-          <div className='flex flex-col'>
-            <div className="m-2 flex items-center">
-              <img className='w-14 rounded-xl' src={profileImage}/>
-              <p className='p-2'>Mohamed Amine Boughnimi</p>
+          <DropdownMenu>
+            <div className='flex flex-col'>
+              <div className="m-2 flex items-center">
+                <img className='w-14 rounded-xl' src={profileImage} />
+                <p className='p-2'>Mohamed Amine Boughnimi</p>
+                <DropdownMenuTrigger><TiThMenuOutline size={22} /></DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>Profile</DropdownMenuItem>
+                  <DropdownMenuItem>Billing</DropdownMenuItem>
+                  <DropdownMenuItem>Team</DropdownMenuItem>
+                  <DropdownMenuItem>Subscription</DropdownMenuItem>
+                  </DropdownMenuContent>
+              </div>
             </div>
-          </div>
           </DropdownMenu>
-
         </SidebarFooter>
       </Sidebar>
       <div className="App">
